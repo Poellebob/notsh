28b207db373302f94cda752ca13ef79d9d7982a2
Viggo Kirkegaard Helstrup <vkirkegaardhelstrup@gmail.com>
2026-06-08 18:13:14 +0200

    fix flake

--- flake.nix
+++ flake.nix
@@ -3,26 +3,24 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        packages = {
-          notsh = pkgs.callPackage ./default.nix { };
-          default = self.packages.${system}.notsh;
-        };
+  outputs = { self, nixpkgs }: let
+    supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+    forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system nixpkgs.legacyPackages.${system});
+  in {
+    packages = forAllSystems (system: pkgs: {
+      notsh = pkgs.callPackage ./default.nix { };
+      default = self.packages.${system}.notsh;
+    });
 
-        apps = {
-          notsh = { type = "app"; program = "${self.packages.${system}.notsh}/bin/note"; };
-          default = self.apps.${system}.notsh;
-        };
-      });
+    apps = forAllSystems (system: pkgs: {
+      notsh = { type = "app"; program = "${self.packages.${system}.notsh}/bin/note"; };
+      default = self.apps.${system}.notsh;
+    });
 
-  overlays.default = final: prev: {
-    notsh = final.callPackage ./default.nix { };
+    overlays.default = final: prev: {
+      notsh = final.callPackage ./default.nix { };
+    };
   };
 }
